6b8a6f311f08d5b01aeb2a3c30ec159f5d033ad9
lrayne <legaterayne@gmail.com>
2025-07-10 20:58:23 +0300

    feat: add contact enums

---
+++ app/data/enums.py
@@ -0,0 +1,26 @@
+from enum import Enum
+
+
+class Gender(Enum):
+    male = 'Мужчина'
+    female = 'Женщина'
+    other = '...'
+
+
+class Priority(Enum):
+    regular = 'Обычный'
+    high = 'Особой важности'
+
+
+class Category(Enum):
+    not_selected = 'Не выбрана'
+    friends = 'Друзья'
+    family = 'Семья'
+    work = 'Работа'
+    clients = 'Клиенты'
+
+
+class Channel(Enum):
+    can_call = 'Можно звонить'
+    can_message = 'Можно писать сообщения'
+    email_preferred = 'Предпочтительно по эл. почте'
