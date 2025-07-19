63b082c1832d96f474cf711280b5a1ea8ec1d207
lrayne <legaterayne@gmail.com>
2025-07-20 00:23:55 +0300

    feat: implement routes

--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,7 @@
 # pyright: reportUnusedFunction=false
+import random
+from datetime import datetime
+
 from flask import (
     Flask,
     abort,
@@ -9,14 +12,13 @@ from flask import (
     request,
     url_for,
 )
-from werkzeug.datastructures.structures import ImmutableMultiDict
 
 from app.common import date_to_str
 from app.data.repository import Contact, ContactsRepository
 from app.validators import ContactValidator
 
 repo = ContactsRepository()
-repo.generate_contacts_data()
+repo.generate_contacts_data(1000)
 
 
 def init_app(app: Flask):
@@ -28,6 +30,7 @@ def init_app(app: Flask):
         action = request.args.get('action')
         query = request.args.get('query', '')
         messages = get_flashed_messages(with_categories=True)
+        page = request.args.get('page', 1, type=int)
 
         if action == 'reset':
             contacts = repo.read()
@@ -36,19 +39,62 @@ def init_app(app: Flask):
         else:
             contacts = repo.find_by_name_or_last_name(query)
 
+        contacts = sorted(
+            contacts,
+            key=lambda c: (
+                (c.last_name or '').lower(),
+                (c.first_name or '').lower(),
+            ),
+        )
+
+        search_placeholder = random.choice(
+            [
+                'Ну этот, как его...',
+                'Ну эта, как ее...',
+                'Тот, который...',
+                'Та, которая...',
+                'Человек с именем...',
+            ]
+        )
+
+        total = len(contacts)
+        per_page = 10
+        pages = (total + per_page - 1) // per_page
+        start = (page - 1) * per_page
+        end = start + per_page
+        contacts_page = contacts[start:end]
+
+        current_year = datetime.now().year
+
         return render_template(
             'contacts/index.html',
             search=query,
-            contacts=contacts,
+            search_placeholder=search_placeholder,
+            contacts=contacts_page,
+            current_year=current_year,
             messages=messages,
+            page=page,
+            pages=pages,
         )
 
     @app.route('/contacts/<id>')
     def contacts_show(id: str):
         contact = repo.find_by_id(id)
+        messages = get_flashed_messages(with_categories=True)
 
         if contact is None:
-            abort(404, description='Contact not found')
+            abort(404, description='Контакты не найдены')
+
+        return render_template(
+            'contacts/show.html',
+            contact=contact,
+            date_to_str=date_to_str,
+            messages=messages,
+        )
+
+    @app.route('/contacts/random')
+    def contacts_random_show():
+        contact = random.choice(repo.read())
 
         return render_template(
             'contacts/show.html', contact=contact, date_to_str=date_to_str
@@ -56,23 +102,24 @@ def init_app(app: Flask):
 
     @app.route('/contacts/new')
     def contacts_new():
-        contact: Contact = Contact.empty()
-        errors: dict[str, str] = {}
+        contact = Contact.empty()
 
         return render_template(
-            'contacts/new.html', contact=contact, errors=errors
+            'contacts/new.html', contact=contact, errors={}, form_data={}
         )
 
     @app.post('/contacts')
     def contacts_post():
-        form_data: ImmutableMultiDict[str, str] = request.form
+        form_data = request.form
         validator = ContactValidator(form_data)
         errors = validator.validate()
         contact = Contact.from_form_data(form_data)
 
         if errors:
             return render_template(
-                'contacts/new.html', contact=contact, errors=errors
+                'contacts/new.html',
+                form_data=form_data,
+                errors=errors,
             ), 422
 
         repo.create(contact)
@@ -84,31 +131,36 @@ def init_app(app: Flask):
     @app.route('/contacts/<id>/edit')
     def contacts_edit(id: str):
         contact = repo.find_by_id(id)
-        errors: dict[str, str] = {}
 
         if not contact:
             abort(404, description='Контакт не найден')
 
         return render_template(
-            'contacts/edit.html', contact=contact, errors=errors
+            'contacts/edit.html',
+            contact=contact,
+            errors={},
         )
 
     @app.post('/contacts/<id>')
     def contacts_patch(id: str):
-        form_data: ImmutableMultiDict[str, str] = request.form
-        validator: ContactValidator = ContactValidator(form_data)
+        form_data = request.form
+        validator = ContactValidator(form_data)
         errors = validator.validate()
         contact = repo.find_by_id(id)
 
         if not contact:
             abort(404, description='Контакт не найден')
 
+        updated_contact = Contact.from_form_data(form_data)
+        updated_contact.id = id
+
         if errors:
             return render_template(
-                'contacts/edit.html', contact=contact, errors=errors
+                'contacts/edit.html',
+                form_data=form_data,
+                errors=errors,
+                contact=updated_contact,
             ), 422
-
-        updated_contact = Contact.from_form_data(form_data)
 
         repo.update(
             id=id,
@@ -123,6 +175,8 @@ def init_app(app: Flask):
             channels=updated_contact.channels,
             current_address=updated_contact.current_address,
         )
+
+        flash('Контакт отредактирован', 'success')
 
         return redirect(url_for('contacts_show', id=id))
 
