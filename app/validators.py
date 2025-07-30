6d0df6320d147c0d89a54673336f3243c9c28809
lrayne <legaterayne@gmail.com>
2025-07-30 14:30:53 +0300

    feat: update validator

--- app/validators.py
+++ app/validators.py
@@ -1,13 +1,15 @@
+import re
+
 from werkzeug.datastructures import ImmutableMultiDict
 
 
 class ContactValidator:
-    def __init__(self, data: ImmutableMultiDict[str, str]) -> None:
+    def __init__(self, data: ImmutableMultiDict[str, str]):
         self.data: dict[str, str] = data.to_dict()
         self.errors: dict[str, str] = {}
 
-    def validate_first_name(self) -> None:
-        first_name: str = self.data.get('first_name', '')
+    def validate_first_name(self):
+        first_name = self.data.get('first_name', '')
         if not first_name:
             self.errors['first_name'] = 'Поле "Имя" обязательно для заполнения'
         elif not first_name[0].isupper():
@@ -15,24 +17,29 @@ class ContactValidator:
                 'Имя должно начинаться с заглавной буквы'
             )
 
-    def validate_last_name(self) -> None:
-        last_name: str = self.data.get('last_name', '')
+    def validate_last_name(self):
+        last_name = self.data.get('last_name', '')
         if last_name and not last_name[0].isupper():
             self.errors['last_name'] = (
                 'Фамилия должна начинаться с заглавной буквы'
             )
 
-    def validate_phone(self) -> None:
-        phone = self.data.get('phone', '')
+    def validate_phone(self):
+        phone = self.data.get('phone', '').strip()
 
         if phone:
-            if phone[0] == '+':
-                digits = phone[1:]
+            cleaned = re.sub(r'[ \-\(\)]', '', phone)
+
+            if cleaned.startswith('+'):
+                digits = cleaned[1:]
             else:
-                digits = phone
+                digits = cleaned
 
             if not digits.isdigit():
-                self.errors['phone'] = 'Телефон должен содержать только цифры'
+                self.errors['phone'] = (
+                    'Телефон должен содержать только цифры'
+                    ' (после "+" разрешены пробелы, скобки и дефисы)'
+                )
 
     def validate(self) -> dict[str, str]:
         self.validate_first_name()
