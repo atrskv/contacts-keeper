687dbfc18809e176aa4048e3622753caa3e6bc95
lrayne <legaterayne@gmail.com>
2025-08-25 21:51:28 +0300

    refactor: rename DB_PASS to DB_PASSWORD

--- app/routes.py
+++ app/routes.py
@@ -25,12 +25,12 @@ _ = load_dotenv()
 DB_HOST = os.getenv('DB_HOST')
 DB_NAME = os.getenv('DB_NAME')
 DB_USER = os.getenv('DB_USER')
-DB_PASS = os.getenv('DB_PASS')
+DB_PASSWORD = os.getenv('DB_PASSWORD')
 
 
 def get_db_connection():
     conn = psycopg2.connect(
-        host=DB_HOST, database=DB_NAME, user=DB_USER, password=DB_PASS
+        host=DB_HOST, database=DB_NAME, user=DB_USER, password=DB_PASSWORD
     )
     return conn
 
